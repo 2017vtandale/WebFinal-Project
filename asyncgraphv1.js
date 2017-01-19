a132d206ed3fb5c196796e9b33c18f5a4f0ed04b
Sishaar Rao <sishaar@gmail.com>
2017-01-19 11:06:22 -0500

    markup and documentation

---
+++ asyncgraphv1.js
@@ -0,0 +1,13 @@
+// Created by Sishaar Rao
+// This is an aynchronous creation of a graph utilizing Chart.js
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the data from the
+ *    database is retrieved and ready to be graphed
+ */
+
+function createGraph() {
+   
+}
