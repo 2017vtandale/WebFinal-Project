6e2b913784dd7f099c01885d2b183dd3a5c702cf
Sishaar Rao <sishaar@gmail.com>
2017-01-23 10:26:21 -0500

    outlining structure

--- asyncgraphv1.js
+++ asyncgraphv1.js
@@ -8,6 +8,24 @@
  *    database is retrieved and ready to be graphed
  */
 
+
+
 function createGraph() {
-   
+   var ctx = document.getElementById("myChart").getContext("2d");
+      var myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+              datasets: [{
+                  label: "My First dataset",
+                  data: [1, 2, 3, 2, 1, 2, 3, 4, 5, 4]
+              }]
+          },
+          options: {}
+      });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    //Get Data (callback function)
+    //   Display data on graph --> end
+});
