eade879200195a17b30f4378c806feda7ac67571
Vishal Tandale <vishalt2000@gmail.com>
2017-01-24 14:26:26 -0500

    User Login

---
+++ login/login.js
@@ -0,0 +1,7 @@
+var login = document.getElementById('loginbut');
+var username = document.getElementById('username');
+var pw = document.getElementById('userpass');
+login.onclick = function(){
+  un = username.value;
+  pw = pw.value;
+}
