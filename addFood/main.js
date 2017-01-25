acd609d9719f088444b4bfbf0ab2c71252126c37
Vishal Tandale <vishalt2000@gmail.com>
2017-01-24 21:05:23 -0500

    Add food

--- addFood/main.js
+++ addFood/main.js
@@ -1,14 +1,16 @@
 var loginbut = document.getElementById('loginbut');
 var food = document.getElementById('foodname');
 loginbut.onclick = function(){
+  console.log(food.value);
   var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
+
       if (this.readyState == 4 && this.status == 200) {
         console.log(this.response.Text)
         document.getElementById('foods').innerHTML = this.responseText;
         //console.log("I got the text");
       }
     }
-    xhttp.open("GET", "./getFood.php?food="+food.value, true);
+    xhttp.open("GET", "https://webfinal-project.herokuapp.com/addFood/getFood.php?food="+food.value, true);
     xhttp.send();
 }
