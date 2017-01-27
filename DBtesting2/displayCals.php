ddcd8af1cfa995e79475562340ccfd0969614785
Sishaar Rao <sishaar@gmail.com>
2017-01-27 13:09:53 -0500

    display by user

--- DBtesting2/displayCals.php
+++ DBtesting2/displayCals.php
@@ -1,4 +1,6 @@
 <?php
+   $userID = $_REQUEST["userID"];
+
    $host="host=ec2-54-83-49-44.compute-1.amazonaws.com";
    $dbname="dbname=d5ri1a2h6r334q";
    $user="user=wyvigddvdlnsdi";
@@ -6,7 +8,7 @@
    $password="password=085b65180ffaae50a448d5fc2935f7ef9c49bab3c0b0a3761ce7714e7642a50d";
    $db = pg_pconnect($host." ".$dbname." ".$user." ".$port." ".$password);
 
-   $query = "SELECT * FROM CalorieDataSet";
+   $query = "SELECT * FROM CalorieDataSet WHERE userID='$userID'";
    $ret = pg_query($query);
    if(!$ret){
       echo(pg_last_error($db));
