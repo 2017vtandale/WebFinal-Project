09aaa915bc497582b67b06fc8ad7de484953d13e
Vishal Tandale <vishalt2000@gmail.com>
2017-01-29 14:27:01 -0500

    Food Add

--- addFood/dbTest.php
+++ addFood/dbTest.php
@@ -6,7 +6,7 @@ $port="port=5432";
 $password="password=085b65180ffaae50a448d5fc2935f7ef9c49bab3c0b0a3761ce7714e7642a50d";
    $db = pg_pconnect($host." ".$dbname." ".$user." ".$port." ".$password);
 
-$query = "CREATE TABLE food(username varchar(255), enterdate DATE, calories smallint, protein smallint, carb, smallint, fat smallint )";
+$query = "CREATE TABLE food(username varchar(255), enterdate date, calories smallint, protein smallint, carb, smallint, fat smallint )";
 $ret = pg_query($query);
 if(!$ret){
 echo(pg_last_error($db));
