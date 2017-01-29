4190d634afd066cc5d31dbf3eac2e0d4f605d7ec
Vishal Tandale <vishalt2000@gmail.com>
2017-01-29 12:38:36 -0500

    Register

--- register/main.js
+++ register/main.js
@@ -1,9 +1,1 @@
 
-var login = document.getElementById('loginbut');
-var username = document.getElementById('username');
-var pw = document.getElementById('userpass');
-var register = document.getElementById('Register');
-// login.onclick = function(){
-//   un = username.value;
-//   pw = pw.value;
-// };
