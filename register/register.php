d05ef39ef14c751e885ede05e897356b463b48be
Vishal Tandale <vishalt2000@gmail.com>
2017-01-29 12:11:33 -0500

    Register

---
+++ register/register.php
@@ -0,0 +1,26 @@
+<?php
+  $host="host=ec2-54-235-84-244.compute-1.amazonaws.com";
+  $dbname="dbname=d1ovq05vs2aqii";
+  $user="user=nxamshinupbnkg";
+  $port="port=5432";
+  $password="password=758dac437d2f27d11f6431307baf1bf13569730baa40b8c86cdb77291cb8aea8";
+  $db = pg_pconnect($host." ".$dbname." ".$user." ".$port." ".$password);
+
+  $user = htmlspecialchars($_POST["user"]);
+  $pw = htmlspecialchars($_POST["pw"]);
+  $pw = htmlspecialchars($_POST["pw1"]);
+  if($pw===$pw1){
+    $query = "INSERT INTO login VALUES('$user','$pw')";
+    $ret = pg_query($query);
+    if(!$ret){
+      echo(pg_last_error($db));
+    }
+    else{
+      echo "We Made It"
+      header("Location: ../login/login.html");
+    }
+  }
+  else{
+    header("Location: ./index.html");
+  }
+?>
