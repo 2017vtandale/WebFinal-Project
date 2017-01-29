db07a45e92f0ea02a273f6a7ce30895ede2ac5d8
Vishal Tandale <vishalt2000@gmail.com>
2017-01-29 12:42:29 -0500

    Register

--- register/register.php
+++ register/register.php
@@ -12,7 +12,7 @@
   $port="port=5432";
   $password="password=758dac437d2f27d11f6431307baf1bf13569730baa40b8c86cdb77291cb8aea8";
   $db = pg_pconnect($host." ".$dbname." ".$user." ".$port." ".$password);
-
+  echo("I'm Here");
   $user = htmlspecialchars($_POST["user"]);
   $pw = htmlspecialchars($_POST["pw"]);
   $pw = htmlspecialchars($_POST["pw1"]);
