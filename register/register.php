4960d9c2978060dcfcbc67e7678f274375831461
Vishal Tandale <vishalt2000@gmail.com>
2017-01-29 12:50:14 -0500

    Register

--- register/register.php
+++ register/register.php
@@ -12,10 +12,10 @@
   $port="port=5432";
   $password="password=758dac437d2f27d11f6431307baf1bf13569730baa40b8c86cdb77291cb8aea8";
   $db = pg_pconnect($host." ".$dbname." ".$user." ".$port." ".$password);
-  echo("I'm Here");
+  echo("I'm Here" );
   $user = htmlspecialchars($_POST["user"]);
   $pw = htmlspecialchars($_POST["pw"]);
-  $pw = htmlspecialchars($_POST["pw1"]);
+  $pw1 = htmlspecialchars($_POST["pw1"]);
   echo($pw." ".$pw1);
   if($pw==$pw1){
     $query = "INSERT INTO login VALUES('$user','$pw')";
@@ -24,10 +24,10 @@
     }
     else{
       echo("We Made it");
-      //redirect("../login/login.html",false);
+      redirect("../login/login.html",false);
     }
   }
   else{
-    //redirect("./index.html",false);
+    redirect("./index.html",false);
   }
 ?>
