a18b986eb045eb901c3302f647ff0e95bbf62177
Sishaar Rao <sishaar@gmail.com>
2017-01-29 19:26:53 -0500

    implemented a UI method of adding data

--- dashboard/home.php
+++ dashboard/home.php
@@ -202,7 +202,7 @@
             }
             xmlhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    window.location.replace(window.location.href+"user="+userID);
+                    window.location = "./home.php?user="+userID);
                 }
             };
 
